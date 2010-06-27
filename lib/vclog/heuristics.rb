6212882725201b1dd7483c35d9d887f408e450ce
7rans <transfire@gmail.com>
2010-06-27 14:29:33 -0400

    rule block can take none, matadata, or message and matachdata

--- lib/vclog/heuristics.rb
+++ lib/vclog/heuristics.rb
@@ -75,14 +75,23 @@ module VCLog
 
     #
     class Rule
+      #
       def initialize(pattern, &block)
         @pattern = pattern
         @block   = block        
       end
 
+      # Process the rule.
       def call(message)
         if matchdata = @pattern.match(message)
-          @block.call(message, matchdata)
+          case @block.arity
+          when 0
+            @block.call
+          when 1
+            @block.call(matchdata)
+          else
+            @block.call(message, matchdata)
+          end
         else
           nil
         end
