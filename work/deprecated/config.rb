4a24d3630716729366f44d619cb0e5c04eb539b7
7rans <transfire@gmail.com>
2010-11-19 17:22:36 -0500

    admin: update metadata and deprecate config.rb

--- work/deprecated/config.rb
+++ work/deprecated/config.rb
@@ -8,14 +8,11 @@ module VCLog
     # File glob used to find the vclog configuration directory.
     CONFIG_GLOB = '{.vclog,.config/vclog,config/vclog}/'
 
-    # File glob used to find project root directory.
-    ROOT_GLOB = '{.vclog/,.config/vclog/,config/vclog/,.git/,README*}/'
-
     #
-    def initialize(root=nil)
-      @root  = root || lookup_root || Dir.pwd
+    def initialize(root, options={})
+      @root  = root
       @dir   = Dir[File.join(root, CONFIG_GLOB)]
-      @level = 0
+      @level = options[:level] || 0
     end
 
     # Project's root directory.
@@ -37,27 +34,6 @@ module VCLog
     #
     def heuristics_file
       @heuristics_file ||= Dir[File.join(dir, 'rules.rb')].first
-    end
-
-    # Find project root. This searches up from the current working
-    # directory for the following paths (in order):
-    #
-    #   .vclog/
-    #   .config/vclog/
-    #   config/vclog/
-    #   .git/
-    #   README*
-    #
-    def lookup_root
-      root = nil
-      Dir.ascend(Dir.pwd) do |path|
-        check = Dir[ROOT_GLOB].first
-        if check
-          root = path 
-          break
-        end
-      end
-      root || Dir.pwd
     end
 
   end
