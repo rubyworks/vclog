a25588432e8bdbfceef992cb991588960ce5d31e
7rans <transfire@gmail.com>
2011-11-06 16:26:52 -0500

    Update HISTORY file. [admin]

---
+++ .co.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+
+vclog do
+  set :major,  1, "Major Enhancements"
+  set :bug,    0, "Bug Fixes"
+  set :minor, -1, "Minor Enhancements"
+  set :doc,   -1, "Documentation Changes"
+  set :admin, -2, "Administrative Changes"
+
+  on /^(\w+):/ do |msg, md|
+    [md[1].to_sym, md.post_match]
+  end
+
+  on /\[(\w+)\]\s*$/ do |msg, md|
+    [md[1].to_sym, md.pre_match]
+  end
+
+  on /updated? (README|PROFILE|PACKAGE|VERSION|MANIFEST)/ do
+    :admin
+  end
+
+  on /(bump|bumped|prepare) version/ do
+    :admin
+  end
+end
