cb2f6a6e626b49ace618ae3e5a6cebcce722e351
7rans <transfire@gmail.com>
2011-12-10 00:20:21 -0500

    Fix metadata test for new var location.

--- test/unit/case_metadata.rb
+++ test/unit/case_metadata.rb
@@ -4,7 +4,7 @@ require 'vclog/metadata'
 testcase "VCLog" do
 
   test "VERSION is set" do
-    ::VCLog::VERSION.assert == File.read('profile/version').strip
+    ::VCLog::VERSION.assert == File.read('var/version').strip
   end
 
 end
