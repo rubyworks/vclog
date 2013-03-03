4a835c9c879333ef578b4a03f9f928ccb4364644
7rans <transfire@gmail.com>
2013-03-03 13:41:23 -0500

    Fix test. [test]

--- test/case_metadata.rb
+++ test/case_metadata.rb
@@ -3,7 +3,7 @@ require 'vclog/metadata'
 testcase "VCLog" do
 
   test "VERSION is set" do
-    ::VCLog::VERSION.assert == File.read('meta/version').strip
+    ::VCLog::VERSION.assert == File.read('index/version').strip
   end
 
 end
